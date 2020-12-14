df460cbdeec1d32c20714bc2d76493a33ba7f44f
Igor Muzyka <igormuzyka42@gmail.com>
2020-12-14 23:37:31 +0200

    Downgraded macOS requirement down to v10_10

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "MenuBar",
     platforms: [
-        .macOS(.v10_15)
+        .macOS(.v10_10),
     ],
     products: [
         .library(name: "MenuBar", targets: ["MenuBar"]),
